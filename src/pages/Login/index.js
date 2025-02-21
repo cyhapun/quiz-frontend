c68345e1a12215e2cda388fc3602662b71598d59
Phuc Chau <cyhapun242@gmail.com>
2025-02-21 08:20:40 +0700

    Fix bug login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,7 +8,7 @@ function Login() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        alert("Wranning");
+        console.log(e);
         const email = e.target[0].value;
         const password = e.target[1].value;
         const response = await login(email, password);
